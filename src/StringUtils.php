0c546f52e8862ef8552b8ae6595398cc673d0f6b
Romain FRANCOIS <romain.francois3@hotmail.fr>
2021-04-13 11:32:15 +0200

    :version: 1.0.4

---
+++ src/StringUtils.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace CkAmaury\PhpMagicFunctions;
+
+
+class StringUtils{
+
+    static function isJSON($string):bool{
+        return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE);
+    }
+}
